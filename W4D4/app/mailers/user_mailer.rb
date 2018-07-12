6f7cb1b9daa7fd72db2e902d54335e50f2372c43
Amanda <a@zalee.me>
2018-07-12 00:05:07 -0700

    Add ActionMailer functionality

--- W4D4/app/mailers/user_mailer.rb
+++ W4D4/app/mailers/user_mailer.rb
@@ -1,5 +1,9 @@
 class UserMailer < ApplicationMailer
   default from: 'from@example.com'
 
-
+  def welcome_email(user)
+    @user = user
+    @url = 'localhost:3000/session/new'
+    mail(to: user.email, subject: 'Welcome to 99Cats!')
+  end
 end
