04af61faffe3dd8afc2619efc0dc14fe4e57ec5b
Amanda <a@zalee.me>
2018-07-31 00:43:12 -0700

    Complete W7D2 Todo App middleware Phase1

--- W7D2/Todo App/frontend/todo_redux.jsx
+++ W7D2/Todo App/frontend/todo_redux.jsx
@@ -4,10 +4,21 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+const addLoggingToDispatch = (store) => {
+  let sd = store.dispatch;
+  return (action) => {
+    console.log("old state: ", store.getState());
+    console.log("action: ", action);
+    sd(action);
+    console.log("new state: ", store.getState());
+  };
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
+  store.dispatch = addLoggingToDispatch(store);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
