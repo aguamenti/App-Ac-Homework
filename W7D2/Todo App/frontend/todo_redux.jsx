0d81b64f123d5dccdf6ba6c0df98c4a55776efe4
Amanda <a@zalee.me>
2018-07-31 00:49:12 -0700

    Complete W7D2 Todo App middleware Phase3: Redux

--- W7D2/Todo App/frontend/todo_redux.jsx
+++ W7D2/Todo App/frontend/todo_redux.jsx
@@ -4,21 +4,39 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
-const addLoggingToDispatch = (store) => {
-  let sd = store.dispatch;
-  return (action) => {
-    console.log("old state: ", store.getState());
-    console.log("action: ", action);
-    sd(action);
-    console.log("new state: ", store.getState());
-  };
-};
+// Phase1: Logging
+// const addLoggingToDispatch = (store) => {
+//   let sd = store.dispatch;
+//   return (action) => {
+//     console.log("old state: ", store.getState());
+//     console.log("action: ", action);
+//     sd(action);
+//     console.log("new state: ", store.getState());
+//   };
+// };
+
+//Phase2: Refactoring
+// const addLoggingToDispatch = store => next => action => {
+//     console.log("old state: ", store.getState());
+//     console.log("action: ", action);
+//     next(action);
+//     console.log("new state: ", store.getState());
+// };
+//
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let sd = store.dispatch;
+//   middlewares.forEach((middleware) => {
+//     sd = middleware(store)(sd);
+//   });
+//   return Object.assign({}, store, { sd });
+// };
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
-  store.dispatch = addLoggingToDispatch(store);
+  // store.dispatch = addLoggingToDispatch(store); //Phase1: Logging
+  // store = applyMiddlewares(store, addLoggingToDispatch); //Phase2: Refactoring
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
