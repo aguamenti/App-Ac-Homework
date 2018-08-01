5142841ca7a5c07bcccb2135bc000f21e3e8a0d4
Amanda <a@zalee.me>
2018-08-01 01:21:05 -0700

    Complete W7D3 GiphysSearch Phase3:React

--- W7D3/GiphySearch/components/giphys_search.jsx
+++ W7D3/GiphySearch/components/giphys_search.jsx
@@ -1,3 +1,35 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch {
+  constructor(props) {
+    this.props = props;
+    this.state = {
+      searchTerm: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({searchTerm: e.currentTarget.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.fetchSearchGiphys(this.state.searchTerm.split(' ').join('+'));
+  }
+
+  render() {
+    return (
+      <form >
+        <input type="text" value={this.state.searchTerm} onChange={this.handleChange}></input>
+        <button type="submit" onClick={this.handleSubmit}>Search</button>
+      </form>
+    );
+  }
+}
+
+export default GiphysSearch;
--- W7D3/GiphySearch/giphy_search.jsx
+++ W7D3/GiphySearch/giphy_search.jsx
@@ -6,6 +6,6 @@ import {fetchSearchGiphys} from './util/api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
-  // const root = document.getElementById('root');
-  // ReactDOM.render(<Root store = {store} />, root);
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store = {store} />, root);
 });
